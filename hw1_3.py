afab74bc80104ed504825498d5f541d044eb3310
Murat Karakuş <59150795+mmuurraatt@users.noreply.github.com>
2022-03-01 00:45:22 +0300

    Add files via upload

---
+++ hw1_3.py
@@ -0,0 +1,26 @@
+from skimage import io, color, data
+import pandas as pd
+from pylab import *
+
+img = io.imread('resim3.jpg')
+print(img.shape)
+
+
+df = pd.DataFrame(img.flatten())
+filepath = 'pixel_values.xlsx'
+df.to_excel(filepath, index=False)
+
+io.imshow(img)
+
+# Covert to HSV
+img_hsv = color.rgb2hsv(img)
+# Covert back to RGB
+img_rgb = color.hsv2rgb(img_hsv)
+
+# Show both figures
+figure(0)
+io.imshow(img_hsv)
+figure(1)
+io.imshow(img_rgb)
+
+io.show()
